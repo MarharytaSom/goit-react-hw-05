d236be694137d4f7079e206ec445f8caaccb01f1
MarharytaSom <158225902+MarharytaSom@users.noreply.github.com>
2024-10-28 00:17:10 +0100

    delete c n r

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Navigation from './components/Navigation/Navigation';
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
-const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
-const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 const App = () => {
